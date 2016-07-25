c0a2356218709337da88a99ed31c9d4e85d17fee
ryanaustinf <ryanaustinf@yahoo.com>
2016-07-25 17:43:14 +0800

    Close Connections

--- src/dao/ItemManager.java
+++ src/dao/ItemManager.java
@@ -20,6 +20,7 @@ public class ItemManager {
 		ps.setString(3, description);
 		ps.setDouble(4,price);
 		ps.execute();
+		con.close();
 	}
 	
 	public static void deleteItem(int id) throws SQLException {
@@ -28,6 +29,7 @@ public class ItemManager {
 		PreparedStatement ps = con.prepareStatement(sql);
 		ps.setInt(1,id);
 		ps.execute();
+		con.close();
 	}
 	
 	public static void editItem(int id, int type, String name, String description, double price) throws SQLException {
@@ -40,6 +42,7 @@ public class ItemManager {
 		ps.setDouble(4,price);
 		ps.setInt(5,id);
 		ps.execute();
+		con.close();
 	}
 	
 	public static Item[] getAllItems(Integer type, String query, int start, int limit) throws SQLException {
@@ -81,25 +84,32 @@ public class ItemManager {
 						rs.getString("itemtype"),rs.getString("description"),
 						rs.getDouble("price")));
 		}
+		con.close();
 		return items.toArray(new Item[0]);
 	}
 	
 	public static Item getItem(int id) throws SQLException {
 		Connection con = DBManager.getInstance().getConnection();
-		String sql = "SELECT I.id, I.name, T.id as typeId, T.name AS itemtype, description, price "
-				+ "FROM tl_item I INNER JOIN tl_item_type T ON I.itemtype = T.id AND "
-				+ "I.status = 1 AND T.status = 1 "
-				+ "WHERE I.id = ?";
-		PreparedStatement ps = con.prepareStatement(sql);
-		ps.setInt(1,id);
-		ResultSet rs = ps.executeQuery();
-		
-		if(rs.next()) {
-			return new Item(rs.getInt("id"),rs.getString("name"),
-						rs.getInt("typeId"),rs.getString("itemtype"),rs.getString("description"),
-						rs.getDouble("price"));
+		try {
+			String sql = "SELECT I.id, I.name, T.id as typeId, T.name AS itemtype, description, price "
+					+ "FROM tl_item I INNER JOIN tl_item_type T ON I.itemtype = T.id AND "
+					+ "I.status = 1 AND T.status = 1 "
+					+ "WHERE I.id = ?";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1,id);
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				return new Item(rs.getInt("id"),rs.getString("name"),
+							rs.getInt("typeId"),rs.getString("itemtype"),rs.getString("description"),
+							rs.getDouble("price"));
+			}
+			return null;
+		} catch(Exception e) {
+			throw e;
+		} finally {
+			con.close();
 		}
-		return null;
 	}
 	
 	public static void purchaseItem(int userId, int itemId, int quantity) throws SQLException {
@@ -110,6 +120,7 @@ public class ItemManager {
 		ps.setInt(2,itemId);
 		ps.setInt(3, quantity);
 		ps.execute();
+		con.close();
 	}
 	
 	public static void reviewItem(int userId, int itemId, int rating, String review) throws SQLException {
@@ -121,70 +132,95 @@ public class ItemManager {
 		ps.setInt(3, rating);
 		ps.setString(4,review);
 		ps.execute();
+		con.close();
 	}
 	
 	public static double getTotalSales() throws SQLException {
 		Connection con = DBManager.getInstance().getConnection();
-		String sql = "SELECT SUM(quantity * price) AS TotalSales "
-				+ "FROM tl_purchase P INNER JOIN tl_item I ON P.itemId = I.id AND "
-				+ "P.status = 1 AND I.status = 1";
-		PreparedStatement ps = con.prepareStatement(sql);
-		ResultSet rs = ps.executeQuery();
-		if( rs.next() ) {
-			return rs.getDouble("TotalSales");
+		try {
+			String sql = "SELECT SUM(quantity * price) AS TotalSales "
+					+ "FROM tl_purchase P INNER JOIN tl_item I ON P.itemId = I.id AND "
+					+ "P.status = 1 AND I.status = 1";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			if( rs.next() ) {
+				return rs.getDouble("TotalSales");
+			}
+			return 0;
+		} catch(Exception e) {
+			throw e;
+		} finally {
+			con.close();
 		}
-		return 0;
 	}
 	
 	public static SaleMapping[] getTotalSales(int itemtype) throws SQLException {
 		Connection con = DBManager.getInstance().getConnection();
-		String sql = "SELECT T.name AS ItemType, SUM(quantity * price) AS TotalSales "
-				+ "FROM tl_purchase P INNER JOIN tl_item I ON P.itemId = I.id AND "
-				+ "P.status = 1 AND I.status = 1 "
-				+ "INNER JOIN tl_item_type T ON I.itemtype = T.id AND T.status = 1 "
-				+ "GROUP BY T.id "
-				+ "ORDER BY TotalSales DESC";
-		PreparedStatement ps = con.prepareStatement(sql);
-		ResultSet rs = ps.executeQuery();
-		ArrayList<SaleMapping> sm = new ArrayList<SaleMapping>();
-		while( rs.next() ) {
-			sm.add(new SaleMapping(rs.getString("ItemType"),rs.getDouble("TotalSales")));
+		try {
+			String sql = "SELECT T.name AS ItemType, SUM(quantity * price) AS TotalSales "
+					+ "FROM tl_purchase P INNER JOIN tl_item I ON P.itemId = I.id AND "
+					+ "P.status = 1 AND I.status = 1 "
+					+ "INNER JOIN tl_item_type T ON I.itemtype = T.id AND T.status = 1 "
+					+ "GROUP BY T.id "
+					+ "ORDER BY TotalSales DESC";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			ArrayList<SaleMapping> sm = new ArrayList<SaleMapping>();
+			while( rs.next() ) {
+				sm.add(new SaleMapping(rs.getString("ItemType"),rs.getDouble("TotalSales")));
+			}
+			return sm.toArray(new SaleMapping[0]);
+		} catch(Exception e) {
+			throw e;
+		} finally {
+			con.close();
 		}
-		return sm.toArray(new SaleMapping[0]);
 	}
 	
 	public static SaleMapping[] getTotalSalesPerItem() throws SQLException {
 		Connection con = DBManager.getInstance().getConnection();
-		String sql = "SELECT I.name, SUM(quantity * price) AS TotalSales "
-				+ "FROM tl_purchase P INNER JOIN tl_item I ON P.itemId = I.id AND "
-				+ "P.status = 1 AND I.status = 1 "
-				+ "GROUP BY I.id "
-				+ "ORDER BY TotalSales DESC";
-		PreparedStatement ps = con.prepareStatement(sql);
-		ResultSet rs = ps.executeQuery();
-		ArrayList<SaleMapping> sm = new ArrayList<SaleMapping>();
-		while( rs.next() ) {
-			sm.add(new SaleMapping(rs.getString("name"),rs.getDouble("TotalSales")));
+		try {
+			String sql = "SELECT I.name, SUM(quantity * price) AS TotalSales "
+					+ "FROM tl_purchase P INNER JOIN tl_item I ON P.itemId = I.id AND "
+					+ "P.status = 1 AND I.status = 1 "
+					+ "GROUP BY I.id "
+					+ "ORDER BY TotalSales DESC";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			ArrayList<SaleMapping> sm = new ArrayList<SaleMapping>();
+			while( rs.next() ) {
+				sm.add(new SaleMapping(rs.getString("name"),rs.getDouble("TotalSales")));
+			}
+			return sm.toArray(new SaleMapping[0]);
+		} catch(Exception e) {
+			throw e;
+		} finally {
+			con.close();
 		}
-		return sm.toArray(new SaleMapping[0]);
 	}
 	
 	public static Review[] getReviews(int itemId, int start, int limit) throws SQLException {
 		Connection con = DBManager.getInstance().getConnection();
-		String sql = "SELECT username, rating, review "
-				+ "FROM tl_review R INNER JOIN tl_user U ON R.userId = U.id AND R.status = 1 AND U.status = 1 "
-				+ "WHERE R.itemId = ? "
-				+ "ORDER BY R.dateAdded DESC "
-				+ "LIMIT ?,?";
-		PreparedStatement ps = con.prepareStatement(sql);
-		ps.setInt(1,itemId);
-		ps.setInt(2,start);
-		ps.setInt(3,limit);
-		ResultSet rs = ps.executeQuery();
-		ArrayList<Review> reviews = new ArrayList<Review>();
-		while(rs.next()) {
-			reviews.add(new Review(rs.getString("username"),rs.getInt("rating"),rs.getString("review")));
+		try {
+			String sql = "SELECT username, rating, review "
+					+ "FROM tl_review R INNER JOIN tl_user U ON R.userId = U.id AND R.status = 1 AND U.status = 1 "
+					+ "WHERE R.itemId = ? "
+					+ "ORDER BY R.dateAdded DESC "
+					+ "LIMIT ?,?";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1,itemId);
+			ps.setInt(2,start);
+			ps.setInt(3,limit);
+			ResultSet rs = ps.executeQuery();
+			ArrayList<Review> reviews = new ArrayList<Review>();
+			while(rs.next()) {
+				reviews.add(new Review(rs.getString("username"),rs.getInt("rating"),rs.getString("review")));
+			}
+			return reviews.toArray(new Review[0]);
+		} catch(Exception e) {
+			throw e;
+		} finally {
+			con.close();
 		}
-		return reviews.toArray(new Review[0]);
 	}
 }
